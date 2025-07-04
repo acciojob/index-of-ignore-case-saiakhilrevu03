bc03fe153e8622f43d35e8a06141090ad7c8bef9
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-07-04 20:24:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function indexOfIgnoreCase(s1, s2) {
 	return main.indexOf(result);  
 }
 
-Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-// alert(indexOfIgnoreCase(s1, s2));
+
+alert(indexOfIgnoreCase(s1, s2));
